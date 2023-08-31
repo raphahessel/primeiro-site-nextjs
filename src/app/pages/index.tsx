e287ec772e5c5878ff841a4e620b9daa2b730801
Raphael Hessel <raphahessel@gmail.com>
2023-08-30 23:16:59 -0300

    testes

---
+++ src/app/pages/index.tsx
@@ -0,0 +1,30 @@
+import Link from 'next/link'
+ 
+function Home() {
+  return (
+    <ul>
+      <li>
+        <Link
+          href={{
+            pathname: '/about',
+            query: { name: 'test' },
+          }}
+        >
+          About us
+        </Link>
+      </li>
+      <li>
+        <Link
+          href={{
+            pathname: '/blog/[slug]',
+            query: { slug: 'my-post' },
+          }}
+        >
+          Blog Post
+        </Link>
+      </li>
+    </ul>
+  )
+}
+ 
+export default Home
